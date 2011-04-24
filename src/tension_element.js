f83da4be0acd26b69defefe5127ba35721f4ca7b
Daniel Hoey <danielhoey1@gmail.com>
2011-04-24 17:17:43 +1000

    create Vector object

--- src/tension_element.js
+++ src/tension_element.js
@@ -2,21 +2,15 @@
 var TensionElement = {
   create: function(data) { 
             data.force = function() {
-              var force_scalar = -this.elasicity * this.stretch();
-              return Vector.scale(force_scalar, Vector.unit(this.vector()));
+              return this.vector().unit().scale(-this.elasicity * this.stretch());
             }
             data.vector = function() {
-              var p = this.position;
-              return [p[1][0] - p[0][0], p[1][1]-p[0][1], p[1][2]-p[0][2]];
+              return Vector.create(this.position);
             }
             data.stretch = function() {
-              var diff = length(this.vector()) - this.length;
+              var diff = this.vector().length - this.length;
               if (diff < 0) { diff = 0; }
               return diff;
-            }
-
-            function length(vector) {
-              return Vector.length(vector);
             }
 
             return data; 
@@ -24,15 +18,21 @@ var TensionElement = {
 };
 
 var Vector = {
-  scale: function(k, v) {
-           return [k*v[0], k*v[1], k*v[2]];
-         },
-  unit: function(v) {
-          var l = this.length(v);
-          return [v[0]/l, v[1]/l, v[2]/l];
-        },
-  length: function(v) {
-            return Math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
-          }
+  create: function(end_points) {
+            var p = end_points;
+            var v = [p[1][0] - p[0][0], p[1][1]-p[0][1], p[1][2]-p[0][2]];
+            var l = length(v);
 
-}
+            var object = {
+              length: l,
+              unit: function() { return Vector.create([[0,0,0], [v[0]/l, v[1]/l, v[2]/l]]); },
+              scale: function(k) { return [k*v[0], k*v[1], k*v[2]]; },
+              data: v
+            };
+
+  
+            function length(v) { return Math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]); }
+
+            return object;
+        }
+};
--- src/tension_element_test.js
+++ src/tension_element_test.js
@@ -3,7 +3,7 @@ load("src/tension_element.js");
 module("TensionElement");
 test("position vector", function() {
   element= TensionElement.create({position:[[0,0,0], [11,0,0]]});
-  deepEqual(element.vector(), [11,0,0]);
+  deepEqual(element.vector().data, [11,0,0]);
 });
 test("elasticity - one dimension", function() {
   element = TensionElement.create({length: 10, elasicity: 4});
